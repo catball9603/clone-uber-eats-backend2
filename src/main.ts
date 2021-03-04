4044cd5e19735c83a1d64e2cf5706a61d05a6965
Hokmah-ym <hokmahgrace@gamil.com>
2021-03-04 15:28:02 +0900

    modify

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4000;
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(process.env.PORT || 4000);
+  await app.listen(PORT);
 }
 bootstrap();
